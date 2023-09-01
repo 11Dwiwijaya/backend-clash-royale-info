e118e5b9d1517e68cefa9731c2b7e45b22092864
Dwi Wjiaya <11dwiwijaya@gmail.com>
2023-09-01 21:24:22 +0700

    first commit

---
+++ pages/api/upcomingchests.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API_KEY = process.env.CLASH_ROYALE_APIKEY
+
+export default async function handler(req, res) {
+    try {
+        const playerTag = req.query.tag;
+
+        const response = await axios.get(`https://proxy.royaleapi.dev/v1/players/%${playerTag}/upcomingchests`, {
+            headers: {
+                'Authorization': `Bearer ${API_KEY}`
+            }
+        });
+
+        const data = response.data;
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Something went wrong' });
+    }
+}
