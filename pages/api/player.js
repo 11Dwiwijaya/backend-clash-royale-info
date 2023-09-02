6330eaf01b742f7737398f8a5afd23f39b539fb5
Dwi Wjiaya <11dwiwijaya@gmail.com>
2023-09-02 19:57:11 +0700

    fix/param player tag

--- pages/api/player.js
+++ pages/api/player.js
@@ -5,8 +5,8 @@ const API_KEY = process.env.CLASH_ROYALE_APIKEY
 export default async function handler(req, res) {
     try {
         const playerTag = req.query.tag;
-
-        const response = await axios.get(`https://proxy.royaleapi.dev/v1/players/%${playerTag}`, {
+    
+        const response = await axios.get(`https://proxy.royaleapi.dev/v1/players/${playerTag}`, {
             headers: {
                 'Authorization': `Bearer ${API_KEY}`
             }
